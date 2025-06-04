3f7a2da5e881dfb0b9a16696865d3921dcce0295
HaoHaogithu <1871023875@qq.com>
2025-06-05 02:47:20 +0800

    fix/cozes

--- ask_report.py
+++ ask_report.py
@@ -148,7 +148,7 @@ def get_coze_response(conversationID,chatID):
             break
     return False
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5003)
+    app.run(debug=True, host='0.0.0.0', port=5005)
 
 
     
